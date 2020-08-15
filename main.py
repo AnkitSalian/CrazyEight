dd13c90982cb5bffbb7c774e5ce2ec5ca3a9f4f2
Ankit Salian <ankitsalian9@gmail.com>
2020-08-15 14:25:14 +0530

    Methods added to fetch card to be played down and read rules

--- main.py
+++ main.py
@@ -13,3 +13,22 @@ def create_deck():
             deck.append(f'{face}{suit}')
     shuffle(deck)
     return deck
+
+def fetch_face_card(deck):
+    facedown_card = deck.pop()
+    while True:
+        if facedown_card[0] == '8':
+            deck.insert(int(len(deck) / 2), facedown_card)
+        else:
+            break
+    return facedown_card, deck
+
+def read_rules():
+    with open('rules.txt', 'r') as rule:
+        print(rule.read())
+        while True:
+            resume = input('Enter --resume to continue the game: ')
+            if resume != '--resume':
+                print('Continue reading...')
+            else:
+                return
